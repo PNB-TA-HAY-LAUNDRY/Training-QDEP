ba9f924f72eddbf1321a4b4ac37b6fbf0b91d5f7
harrysudanaa <Lenovo@Ellen-Lockhart>
2024-08-26 15:05:11 +0800

    initial project asset management

---
+++ code/student/UNDIKSA_2024_TI/asset-management/asset-management/src/java/com/dimata/assetmanagement/entity/masterdata/AssetList.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dimata.assetmanagement.entity.masterdata;
+
+import com.dimata.qdep.entity.Entity;
+
+/**
+ *
+ * @author Harry
+ */
+public class AssetList extends Entity {
+    
+}
